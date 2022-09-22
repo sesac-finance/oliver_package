b9b35a4bc9e64a109d35c0e6b7c61f7f380c3aa7
OliverJoo <jjaemoon@nate.com>
2022-09-22 09:41:37 +0900

    add user-agent for selenium

--- subAssignment.py
+++ subAssignment.py
@@ -26,6 +26,7 @@ import logging
 # chrome_options.add_argument("User-Agent:'application/json;charset=utf-8'")
 
 # For Ubuntu
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
